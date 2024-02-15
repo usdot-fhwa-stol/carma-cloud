870f1895dd190ea8ce4b166a01322f726a8f522b
kruegersp <kruegersp@users.noreply.github.com>
2024-02-15 10:41:09 -0600

    Update SimFederate import InetAdress.

--- src/cc/ws/SimFederate.java
+++ src/cc/ws/SimFederate.java
@@ -3,6 +3,7 @@ package cc.ws;
 import java.io.BufferedInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import javax.servlet.ServletConfig;
