8a656049bc14958b99cf1ec4d19ca9a67ce35e3b
dev <paul.k.bourelly@leidos.com>
2024-05-16 11:25:17 -0400

    Fix build/install scripts issues

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -24,6 +24,8 @@ rm apache-tomcat-9.0.83.tar.gz
 mv apache-tomcat-9.0.83 tomcat
 sed -i 's/appBase="webapps"/appBase="webapps\/carmacloud"/g' tomcat/conf/server.xml
 rm -r tomcat/webapps/*
+mv tomcat /opt 
+
 # download and install JDK 21
 cd /tmp
 wget --no-check-certificate -q https://download.java.net/java/GA/jdk21.0.1/415e3f918a1f4062a0074a2794853d0d/12/GPL/openjdk-21.0.1_linux-x64_bin.tar.gz 
