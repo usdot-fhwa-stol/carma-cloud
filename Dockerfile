1acae3c64ab44f58d57ce34eaa1cf91e949b0f48
dev <paul.k.bourelly@leidos.com>
2024-05-16 14:56:20 -0400

    Update

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,15 @@
 FROM ubuntu:22.04
 # update package manager and install prerequisites
-COPY scripts/install_dependencies.sh /home/cc/scripts/install_dependencies.sh
-RUN /home/cc/scripts/install_dependencies.sh
-COPY src /home/cc/src
-COPY lib /home/cc/lib
-COPY web /home/cc/web
-COPY osmbin /home/cc/osmbin
-COPY *.sh /home/cc/
-COPY scripts/build.sh /home/cc/scripts/build.sh
+COPY scripts/install_dependencies.sh /home/carma-cloud/scripts/install_dependencies.sh
+RUN /home/carma-cloud/scripts/install_dependencies.sh
+COPY src /home/carma-cloud/src
+COPY lib /home/carma-cloud/lib
+COPY web /home/carma-cloud/web
+COPY osmbin /home/carma-cloud/osmbin
+COPY *.sh /home/carma-cloud/
+COPY scripts/build.sh /home/carma-cloud/scripts/build.sh
 
 # download carma-cloud source
-RUN /home/cc/scripts/build.sh
+RUN /home/carma-cloud/scripts/build.sh
 ENTRYPOINT [ "/opt/tomcat/bin/catalina.sh", "run" ]
 
--- scripts/build.sh
+++ scripts/build.sh
@@ -5,23 +5,23 @@ set -ex
 export JAVA_HOME="/opt/jdk"
 export TOMCAT_HOME="/opt/tomcat"
 
-cd /home/cc/src/cc/geosrv
+cd /home/carma-cloud/src/cc/geosrv
 gcc -c -std=c11 -fPIC -Wall -I "${JAVA_HOME}/include/" -I "${JAVA_HOME}/include/linux/" -I /tmp/proj/src/ cs2cswrapper.c
 gcc -shared cs2cswrapper.o -lproj -o /usr/local/lib/libcs2cswrapper.so
 
 mkdir -p "${TOMCAT_HOME}/webapps/carmacloud/ROOT" 
 cd /home
-mv cc/web/* ${TOMCAT_HOME}/webapps/carmacloud/ROOT 
+mv carma-cloud/web/* ${TOMCAT_HOME}/webapps/carmacloud/ROOT 
 mkdir -p ${TOMCAT_HOME}/webapps/carmacloud/ROOT/WEB-INF/classes 
-find ./cc/src -name "*.java" > sources.txt 
+find ./carma-cloud/src -name "*.java" > sources.txt 
 /opt/jdk/bin/javac -cp ${TOMCAT_HOME}/lib/servlet-api.jar:cc/lib/commons-compress-1.18.jar:cc/lib/javax.json.jar:cc/lib/json-20210307.jar:cc/lib/keccakj.jar:cc/lib/log4j-api-2.16.0.jar:cc/lib/vector_tile.jar -d ${TOMCAT_HOME}/webapps/carmacloud/ROOT/WEB-INF/classes @sources.txt 
 rm sources.txt 
 gunzip cc/osmbin/*.gz 
-mv cc/lib ${TOMCAT_HOME}/webapps/carmacloud/ROOT/WEB-INF 
-mv cc/osmbin/rop.csv ${TOMCAT_HOME}/webapps/carmacloud 
-mv cc/osmbin/storm.csv ${TOMCAT_HOME}/webapps/carmacloud 
-mv cc/osmbin/units.csv ${TOMCAT_HOME}/webapps/carmacloud 
-mv cc/osmbin ${TOMCAT_HOME}/webapps/carmacloud 
+mv carma-cloud/lib ${TOMCAT_HOME}/webapps/carmacloud/ROOT/WEB-INF 
+mv carma-cloud/osmbin/rop.csv ${TOMCAT_HOME}/webapps/carmacloud 
+mv carma-cloud/osmbin/storm.csv ${TOMCAT_HOME}/webapps/carmacloud 
+mv carma-cloud/osmbin/units.csv ${TOMCAT_HOME}/webapps/carmacloud 
+mv carma-cloud/osmbin ${TOMCAT_HOME}/webapps/carmacloud 
 mv ${TOMCAT_HOME}/webapps/carmacloud/ROOT/WEB-INF/log4j2.properties ${TOMCAT_HOME}/webapps/carmacloud/ROOT/WEB-INF/classes 
 touch ${TOMCAT_HOME}/webapps/carmacloud/event.csv 
 mkdir -p ${TOMCAT_HOME}/work/carmacloud/xodr 
